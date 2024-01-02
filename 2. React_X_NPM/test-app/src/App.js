182c6c88efaf85d5c706af3de0ebcff31b86ad4d
Kartik Bitla <bitlakartik@gmail.com>
2024-01-02 20:12:28 +0530

    making tic-tac-toe

--- 2. React_X_NPM/test-app/src/App.js
+++ 2. React_X_NPM/test-app/src/App.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 // const products = [
 //   {title:"Apple", id:1, isFruit:true},
 //   {title:"Banana", id:2, isFruit:true},
@@ -14,12 +16,72 @@
 //     )
 // }
 
-export default function myButton(){
-  function sendAlert(){
-    alert("Button has been clicked");
+// export default function myButton(){
+//   function sendAlert(){
+//     alert("Button has been clicked");
+//   }
+
+//   return (
+//     <button onClick={sendAlert}>Click Me!</button>
+//   )
+// }
+
+// export default function myApp(){
+//   return (
+//     <div>
+//       Hello this is a test page
+      
+//       <MyButton/> {" "}
+//       <MyButton/> {" "}
+//       <MyButton/> {" "}
+//       <MyButton/> {" "}
+//       <MyButton/> {" "}
+//       <MyButton/> {" "}
+//       <MyButton/> {" "}
+//       <MyButton/> {" "}
+//       <MyButton/> {" "}
+//     </div>
+    
+//   ) 
+// }
+
+// function MyButton(){
+//   let [count, setCount] = useState("Kartik");
+
+//   function increaseCount(){
+//       setCount(prompt());
+//   }
+  
+//   return (
+//     <button onClick={increaseCount}>
+//       Hi, {count}
+//     </button>
+//   );
+// }
+
+
+
+//LIKED BUTTON
+export default function Liked(){
+  let [liked, setLiked] = useState(true);
+
+  function handleChange(e){
+    setLiked(e.target.checked);
   }
 
   return (
-    <button onClick={sendAlert}>Click Me!</button>
-  )
+    <>
+      <label>
+      <input 
+        type="checkbox"
+        checked={liked}
+        onChange={handleChange}
+      />
+        You Liked This
+      </label>
+      <p>You {liked ? "Liked" : "Did not like"} this.</p>
+    </>
+  );
 }
+
+
