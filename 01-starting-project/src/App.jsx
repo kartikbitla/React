37b1f1ede82db37a11a7eeed0ff86732b3d1f5fc
Kartik Bitla <bitlakartik@gmail.com>
2024-01-12 21:10:15 +0530

    Learning react

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,54 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data';
+
+const reactDescriptions = ["Fundamental", "Crucual", "Core"];
+function genRandomInt(max){
+  return Math.floor(Math.random() * (max+1));
+}
+
+function Header(){
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>      
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!!!
+        </p>
+    </header>
+  );
+}
+
+function CoreConcepts({title, description, image}){
+  return (
+    <li>
+      <img src={image} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>            
+            <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
