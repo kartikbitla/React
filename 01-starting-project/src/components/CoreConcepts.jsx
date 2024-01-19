edac7616cac64ed4595c7bdab7c86d5b28870dbb
Kartik Bitla <bitlakartik@gmail.com>
2024-01-20 03:23:12 +0530

    learning react

--- 01-starting-project/src/components/CoreConcepts.jsx
+++ 01-starting-project/src/components/CoreConcepts.jsx
@@ -1,10 +1,17 @@
-export default function CoreConcepts({title, description, image}){
+import { CORE_CONCEPTS } from '../data.js';
+import CoreConcept from './CoreConcept.jsx';
+
+export default function CoreConcepts(){
     return (
-      <li>
-        <img src={image} alt={title}/>
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </li>
-    )
-  }
-  
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>   
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title}{...conceptItem}/>)}         
+            {/* <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/> */}
+          </ul>
+        </section>
+    );
+}
