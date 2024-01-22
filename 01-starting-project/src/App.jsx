b2bcc1cb3d1a1b09f552ab31352672531964ec01
Kartik Bitla <bitlakartik@gmail.com>
2024-01-22 04:45:42 -0700

    Update App.jsx

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,8 +2,7 @@ import Header from './components/Header.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
 import Examples from './components/Examples.jsx';
 
-function App() {
-  
+function App() {  
   return (
     <>
       <Header/>
